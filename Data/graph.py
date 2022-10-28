02adf2cff1801945b098dc38198ed9f27a5d3635
CaiWilliamsDurham <Cai.Williams@durham.ac.uk>
2022-10-28 11:28:21 +0100

    included .gitignore

--- Data/graph.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-data = pd.read_csv('Al.csv')
-plt.errorbar(data['Active Layer Cost Fraction (%)'],data['Average Thickness (m)'],yerr=data['Standard Error'],fmt='o')
-plt.ylabel('Average Al Thickness (m)')
-plt.xlabel('Active Layer Cost Fraction (%)')
-plt.show()
